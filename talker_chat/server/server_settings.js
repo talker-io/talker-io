c3b2cd002fe28c550766ae2aec911b7c126c2bfa
Tarithj <tarithj@gmail.com>
2020-06-16 22:52:25 +0530

    Update server_settings.js

--- talker_chat/server/server_settings.js
+++ talker_chat/server/server_settings.js
@@ -6,11 +6,11 @@ if (require.main === module){
 //server settings
 const server_name = 'testing server'
 const server_description = 'This is a demo description'
-const server_website = 'https://test.com'//leave empty if you dont want to send your server's website
+const server_website = 'https://test.com'//enter server's website(OPTIONAL)
 const server_message_maxLength = 160
 const server_port = "8080"
-const server_location = ""// if wanted you can enter the location of the server
-const server_language = ""// if wanted you can enter the language of the server if left empty it will be en
+const server_location = ""// enter the location of the server(OPTIONAL)
+const server_language = ""// enter the language of the server(Default:en)
 
 
 
