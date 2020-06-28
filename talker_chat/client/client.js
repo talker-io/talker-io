44c703ba32f4bd961222d27b7a6a617cdbf4e008
tarithj <tarithj@gmail.com>
2020-06-28 14:42:11 +0530

    improved code quality

--- talker_chat/client/client.js
+++ talker_chat/client/client.js
@@ -6,20 +6,20 @@ const repl = require("repl");
 const notification = require("node-notifier");
 
 // custom modules
-const config = require("./config")
+const config = require("./config");
 const validator = require("./modules/connectionValidator");
 const logger = require("./modules/talker_logger");
-const prompt = require("prompts")
-const promptTemplates = require("./modules/promptTemplates")
+const prompt = require("prompts");
+const promptTemplates = require("./modules/promptTemplates");
 
 // request
-const request = require("request")
+const request = require("request");
 
 // command line arguments
-let username = null
-let options = null
-username = process.argv[2]
-options = process.argv[3]
+let username = null;
+let options = null;
+username = process.argv[2];
+options = process.argv[3];
 
 // starts spinner
 spinner.color = "cyan";
@@ -31,7 +31,7 @@ function main(socket, serverdata) {
 
     //Sends disconnect message
     socket.on("disconnect", function () {
-        socket.emit("disconnect", username)
+        socket.emit("disconnect", username);
     });
 
     //connect
@@ -39,17 +39,17 @@ function main(socket, serverdata) {
 
     //message
     socket.on("message", (data) => {
-        const {message, username} = data
+        const {message, username} = data;
         logger.message(username + ": " + message.split("\n")[0] + "\n", "green");
 
         // notifications
-        var notification_message = (`${username}: ${message}`)
-        notification_message = notification_message.substring(0, notification_message.length -1)
+        var notification_message = (`${username}: ${message}`);
+        notification_message = notification_message.substring(0, notification_message.length -1);
         notification.notify({
             title: `talker.io ${serverdata.server_name}`,
             message: notification_message,
             sound: true,
-        })
+        });
     });
 
     //message send
@@ -62,14 +62,14 @@ function main(socket, serverdata) {
     });
 
     socket.on("newUser", function (data) {
-        currentUsers = data.currentUsers
-        logger.message_nl(`New user connected Total users ${currentUsers}`, "green")
+        currentUsers = data.currentUsers;
+        logger.message_nl(`New user connected Total users ${currentUsers}`, "green");
     })
 
     //listens for userDisconnected
     socket.on("userDisconnected", function (data) {
-        currentUsers = data.currentUsers
-        logger.message_nl(`someone disconnected. Total users ${currentUsers}`, "red")
+        currentUsers = data.currentUsers;
+        logger.message_nl(`someone disconnected. Total users ${currentUsers}`, "red");
     })
 
     socket.on("connect_failed", function(){
@@ -90,7 +90,7 @@ async function connectionCheck(socket) {
         let ServerDescription = data.server_description;
         let ServerMaxLength = data.server_message_maxLength;
         let ServerWebsite = data.server_website;
-        const DataJson = JSON.stringify(data)
+        const DataJson = JSON.stringify(data);
 
         //runs if data from server is invalid
         if (validator.validate(data) === false){
@@ -108,22 +108,22 @@ async function connectionCheck(socket) {
                     // if server has a website display
                     if (ServerWebsite === "") {}
                     else {
-                        logger.message_nl(`Website: ${ServerWebsite}\n`, "yellow")
+                        logger.message_nl(`Website: ${ServerWebsite}\n`, "yellow");
                     }
 
                     // if servers max length is under 10 display max length to user
                     if (ServerMaxLength < 10){
-                        logger.message_nl(`This servers max message size is ${ServerMaxLength}`)
+                        logger.message_nl(`This servers max message size is ${ServerMaxLength}`);
                     }
                     else{}
 
-                    option(socket, data)
+                    option(socket, data);
                 }
                 else if (response.value === false) {
                     process.exit(22);
                 }
                 else {
-                    process.exit(44)
+                    process.exit(44);
                 }
             })()
         }
@@ -136,7 +136,7 @@ async function connectionCheck(socket) {
             // if server has a website display the website
             if (ServerWebsite === ""){}
             else {
-                logger.message_nl(`Website: ${ServerWebsite}\n`, "yellow")
+                logger.message_nl(`Website: ${ServerWebsite}\n`, "yellow");
             }
 
             // if servers max length is under 10 display max length to user
@@ -145,7 +145,7 @@ async function connectionCheck(socket) {
             }
             else{}
 
-            option(socket, data)
+            option(socket, data);
 
 
 
@@ -168,21 +168,20 @@ function option(socket, data){
     const DataJson = JSON.stringify(data);
 
 
-
     // info option
     if (options === "info") {
-        logger.message_nl(`${ServerName}"s info\n server name: ${ServerName}\n server description: ${ServerDescription}\n server max message size: ${ServerMaxLength}\n server website: ${ServerWebsite}\nuse other option for other data` , "bold")
+        logger.message_nl(`${ServerName}"s info\n server name: ${ServerName}\n server description: ${ServerDescription}\n server max message size: ${ServerMaxLength}\n server website: ${ServerWebsite}\nuse other option for other data` , "bold");
 
-        main(socket, data)
+        main(socket, data);
     }
 
     // name option
     else if(options === "name"){
         (async () => {
-            logger.message_nl(`\nserver name: ${ServerName}` , "bold")
-            await socket.emit("disconnect", username)
+            logger.message_nl(`\nserver name: ${ServerName}` , "bold");
+            await socket.emit("disconnect", username);
             setTimeout(() => {
-                process.exit(0)
+                process.exit(0);
             }, 10);
         })()
     }
@@ -190,10 +189,10 @@ function option(socket, data){
     // description option
     else if(options === "description"){
         (async () => {
-            logger.message_nl(`\n${ServerName}"s description: ${ServerDescription}` , "bold")
-            await socket.emit("disconnect", username)
+            logger.message_nl(`\n${ServerName}"s description: ${ServerDescription}` , "bold");
+            await socket.emit("disconnect", username);
             setTimeout(() => {
-                process.exit(0)
+                process.exit(0);
             }, 10);
         })()
     }
@@ -201,11 +200,11 @@ function option(socket, data){
     // message size option
     else if(options === "messagesize"){
         (async () => {
-            logger.message_nl(`\n${ServerName}"s max message length: ${ServerMaxLength}` , "bold")
+            logger.message_nl(`\n${ServerName}"s max message length: ${ServerMaxLength}` , "bold");
 
-            await socket.emit("disconnect", username)
+            await socket.emit("disconnect", username);
             setTimeout(() => {
-                process.exit(0)
+                process.exit(0);
             }, 10);
         })()
     }
@@ -213,26 +212,26 @@ function option(socket, data){
     // other option
     else if(options === "other"){
         (async () => {
-            logger.message_nl(`\n${ServerName}"s other information:\n${DataJson}` , "bold")
-            logger.message_nl("If some of these information is not showing when using \"info\" option that means that you are using a old version or the server is sending unwanted data" , "bold")
+            logger.message_nl(`\n${ServerName}"s other information:\n${DataJson}` , "bold");
+            logger.message_nl("If some of these information is not showing when using \"info\" option that means that you are using a old version or the server is sending unwanted data" , "bold");
 
-            await socket.emit("disconnect", username)
+            await socket.emit("disconnect", username);
             setTimeout(() => {
-                process.exit(0)
+                process.exit(0);
             }, 10);
         })()
     }
 
-    // country option
+    // location option
     else if(options === "location"){
 
         // runs when server didnt send the location
         if (typeof(ServerLocation) === undefined || typeof(ServerLocation) !== "string" || ServerLocation === ""){
             (async () => {
-                logger.message_nl(`\nThis server didnt provide its location` , "bold")
-                await socket.emit("disconnect", username)
+                logger.message_nl(`\nThis server didnt provide its location` , "bold");
+                await socket.emit("disconnect", username);
                 setTimeout(() => {
-                    process.exit(0)
+                    process.exit(0);
                 }, 10);
             })()
         }
@@ -240,10 +239,10 @@ function option(socket, data){
         // runs when server sent the location
         else{
             (async () => {
-                logger.message_nl(`server"s location: ${ServerLocation}` , "bold")
-                await socket.emit("disconnect", username)
+                logger.message_nl(`server"s location: ${ServerLocation}` , "bold");
+                await socket.emit("disconnect", username);
                 setTimeout(() => {
-                    process.exit(0)
+                    process.exit(0);
                 }, 10);
             })()
         }
@@ -255,10 +254,10 @@ function option(socket, data){
         // runs when server didnt send total number of users
         if (typeof (ServerTotalUsers) === undefined || typeof (ServerTotalUsers) !== "number") {
             (async () => {
-                logger.message_nl(`\nThis server didnt provide its current user count`, "bold")
-                await socket.emit("disconnect", username)
+                logger.message_nl(`\nThis server didnt provide its current user count`, "bold");
+                await socket.emit("disconnect", username);
                 setTimeout(() => {
-                    process.exit(0)
+                    process.exit(0);
                 }, 10);
             })()
         }
@@ -266,10 +265,10 @@ function option(socket, data){
         // runs when server sent the total number of users
         else {
             (async () => {
-                logger.message_nl(`server"s current number of users: ${ServerTotalUsers - 1} (without current connection)`, "bold")
-                await socket.emit("disconnect", username)
+                logger.message_nl(`server"s current number of users: ${ServerTotalUsers - 1} (without current connection)`, "bold");
+                await socket.emit("disconnect", username);
                 setTimeout(() => {
-                    process.exit(0)
+                    process.exit(0);
                 }, 10);
             })()
         }
@@ -279,25 +278,25 @@ function option(socket, data){
     // language option
     else if(options === "language"){
         (async () => {
-            logger.message_nl(`\n${ServerName}"s language: ${ServerLanguage}` , "bold")
+            logger.message_nl(`\n${ServerName}"s language: ${ServerLanguage}` , "bold");
 
-            await socket.emit("disconnect", username)
+            await socket.emit("disconnect", username);
             setTimeout(() => {
-                process.exit(0)
+                process.exit(0);
             }, 10);
         })()
     }
 
     //no option
     else {
-        main(socket, data)
+        main(socket, data);
     }
 
 }
 
 spinner.stop()
 setTimeout(()=>{
-    spinner.text = "connecting to server"
+    spinner.text = "connecting to server";
     const socket = require("socket.io-client")(config.server_ip);
-    connectionCheck(socket)
-},1000)
+    connectionCheck(socket);
+},1000);
